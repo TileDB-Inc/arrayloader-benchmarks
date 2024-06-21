542b69f1ff9b9764b48b5a6f61215f3a9f257261
Ryan Williams <ryan.williams@tiledb.com>
2024-06-21 15:19:29 -0400

    init-instance tweak

--- notebooks/init-instance.sh
+++ notebooks/init-instance.sh
@@ -87,7 +87,7 @@ install_conda
 ssh-keyscan -t ecdsa github.com >> .ssh/known_hosts
 git clone -b "$branch" --recurse-submodules git@github.com:ryan-williams/arrayloader-benchmarks.git
 cd arrayloader-benchmarks
-echo "cd arrayloader-benchmarks" >> ~/.bash_profile
+echo "cd ~/arrayloader-benchmarks" >> ~/.bash_profile
 
 # Install/Configure Conda+env
 env=arrayloader-benchmarks
