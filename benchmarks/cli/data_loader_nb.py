0b47ed500a95ae2ba56ed2c316148a138e3a6f2b
Ryan Williams <ryan.williams@tiledb.com>
2024-06-22 19:19:52 -0400

    improve plot customdata/hovertemplate, support multiple instance_types in one nb/plot

--- benchmarks/cli/data_loader_nb.py
+++ benchmarks/cli/data_loader_nb.py
@@ -13,13 +13,14 @@ from benchmarks.data_loader.paths import NB_PATH, NB_DIR, DEFAULT_PQT_PATH
 @cli.command()
 @option('-D', '--dataset-slice', callback=lambda ctx, param, value: DatasetSlice.parse(value) if value else None, help="Filter to DB entries matching this URI")
 @option('-h', '--hostname-rgx', help='Filter to DB entries matching this hostname regex')
-@option('-n', '--max-batches', type=int, default=0, help='Optional: Filter to DB entries with this `max_batch` set')
+@option('-i', '--instance-type', help='Optional: filter to DB entries run on this EC2 `instance_type`')
+@option('-n', '--max-batches', type=int, default=0, help='Optional: filter to DB entries with this `max_batch` set')
 @option('-o', '--out-dir', help='Directory (under -O/--out-root) to write the executed notebook – and associated plot data – to')
 @option('-O', '--out-root', default=NB_DIR, help=f'Output "root" directory, default: {NB_DIR}')
 @option('-s', '--since', help="Filter to DB entries run since this datetime (inclusive)")
 @option('--s3/--no-s3', is_flag=True, default=None, help="If set, filter to DB entries run against S3, or run locally")
 @argument('db_path', default=DEFAULT_PQT_PATH)
-def data_loader_nb(db_path, dataset_slice: DatasetSlice, hostname_rgx, max_batches, out_dir: str, out_root, since, s3):
+def data_loader_nb(db_path, dataset_slice: DatasetSlice, hostname_rgx, instance_type, max_batches, out_dir: str, out_root, since, s3):
     nb_path = NB_PATH
     if not out_dir:
         if dataset_slice:
@@ -41,6 +42,7 @@ def data_loader_nb(db_path, dataset_slice: DatasetSlice, hostname_rgx, max_batch
     parameters = dict(
         db_path=db_path,
         hostname_rgx=hostname_rgx,
+        instance_type=instance_type,
         out_dir=out_dir,
         show='png',
         max_batches=max_batches,
